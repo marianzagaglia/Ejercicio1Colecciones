5e05f62580bda18e90388589c7d9ac2039a7c62d
Margarita Bravo <bravo_m33@hotmail.com>
2022-02-11 23:59:12 -0300

    modificacion metodo buscar/eliminar

--- src/ServicioPerro/PerroServicio.java
+++ src/ServicioPerro/PerroServicio.java
@@ -16,42 +16,41 @@ import java.util.Scanner;
  * @author Mariana
  */
 public class PerroServicio {
-    
-   private ArrayList <String> razas;
-   private Scanner leer;
+
+    private ArrayList<String> razas;
+    private Scanner leer;
 
     public PerroServicio() {
-        this.razas= new ArrayList<>();
-        this.leer= new Scanner(System.in);
+        this.razas = new ArrayList<>();
+        this.leer = new Scanner(System.in);
     }
-   
-      
-    public void pedirRaza(){
+
+    public void pedirRaza() {
         String respuesta;
-        int contador=1;
+        int contador = 1;
         do {
-            System.out.println("Ingrese la raza de la mascota "+contador); 
+            System.out.println("Ingrese la raza de la mascota " + contador);
             contador++;
-            String leerraza=leer.nextLine();
+            String leerraza = leer.nextLine();
             razas.add(leerraza);
             System.out.println("¿Desea continuar cargando razas? si/no");
-            respuesta=leer.nextLine();
+            respuesta = leer.nextLine();
         } while (respuesta.equals("si"));
     }
-    
+
     public void mostrarRaza() {
         System.out.println("Las razas de esta lista son: ");
         for (String aux : razas) {
             System.out.println(aux);
         }
-        System.out.println("La cantidad de razas ingresadas es: "+razas.size());
+        System.out.println("La cantidad de razas ingresadas es: " + razas.size());
     }
-    
-    public void buscarRaza() {
-        System.out.println("Ingrese la raza a buscar");
-        String razabuscada=leer.nextLine();
-        razas.remove(razabuscada);
-     Collections.sort(razas);
+
+//    public void buscarRaza() {
+//        System.out.println("Ingrese la raza a buscar");
+//        String razabuscada = leer.nextLine();
+//        razas.remove(razabuscada);
+//        Collections.sort(razas);
 //        Iterator<String> buscar = razas.iterator();
 //     
 //        while (buscar.hasNext()) {
@@ -59,11 +58,23 @@ public class PerroServicio {
 //            if (razabuscada.equals(buscar.next())) {
 //                buscar.remove();
 //                Collections.sort(razas);
+//metodo buscar raza, colaboracion Margarita
+    public void buscarRaza() {
+        System.out.println("Ingrese la raza deperro que desea eliminar");
+        String razaBuscada = leer.nextLine();
+        String raz = "";
+        for (int i = 0; i < razas.size(); i++) {
+            raz = razas.get(i);
+            if (raz.equals(razaBuscada)) {
+                razas.remove(raz);
             }
-        
-            
         }
-    
-    
-    
+        if (raz.equals(razaBuscada) == true) {
+            razas.remove(raz);
+        } else {
+            System.out.println("La raza no fue encontrada");
+        }
 
+    }
+
+}
